36226ffbb57f9774af5fd0293bc8eefdd8ffc67c
skobraf <skobraf@gmail.com>
2019-05-06 13:29:43 +0100

    displayed todos

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,10 +1,20 @@
 <template>
-    
+    <div>
+        <h3>Todos</h3>
+        <div class="todos">
+            <div v-for="todo in allTodos" :key="todo.id" class="todo">
+                {{ todo.title }}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex';
 
+export default {
+    name: "Todos",
+    computed: mapGetters(['allTodos'])
 }
 </script>
 
