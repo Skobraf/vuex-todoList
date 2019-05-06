51df61b05b149434e457fe3aa5ae9f8deaf69662
skobraf <skobraf@gmail.com>
2019-05-06 13:43:52 +0100

    fetch api on created

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,20 +1,17 @@
 import axios from 'axios'
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo One'
-        },
-        {
-            id: 2,
-            title: 'Todo Two'
-        }
-    ]
+    todos: []
 };
 
 const getters = {
     allTodos: state => state.todos
+};
+
+const actions = {
+    async fetchTodos({ commit }) {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+    }
 };
 
 const mutations = {};
