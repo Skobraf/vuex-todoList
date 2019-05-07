a9c035c6a0730b7c401f57272bf4a8290697251c
skobraf <skobraf@gmail.com>
2019-05-07 10:56:22 +0100

    addTodo functionality

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,13 @@
 
 <script>
 import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
 
 export default {
   name: 'app',
   components: {
-    Todos 
+    Todos,
+    AddTodo
   }
 }
 </script>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>Add Todo</h3>
+        <div class="add">
+            <form @submit="handleTodo">
+                <input type="text" v-model="title" placeholder="Add Todo...">
+                <input type="submit" value="submit">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        ...mapActions(['addTodo']),
+        handleTodo(e) {
+            e.preventDefault();
+            this.addTodo(this.title);
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
