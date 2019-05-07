0632a319e127abb2a872bf4f78f68f2fda202f49
skobraf <skobraf@gmail.com>
2019-05-07 11:22:25 +0100

    remove todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,6 +4,7 @@
         <div class="todos">
             <div v-for="todo in allTodos" :key="todo.id" class="todo">
                 {{ todo.title }}
+                <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
             </div>
         </div>
     </div>
@@ -15,7 +16,7 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
     name: "Todos",
     methods: {
-        ...mapActions(["fetchTodos"])
+        ...mapActions(["fetchTodos", "deleteTodo"])
     },
     computed: mapGetters(['allTodos']),
     created() {
